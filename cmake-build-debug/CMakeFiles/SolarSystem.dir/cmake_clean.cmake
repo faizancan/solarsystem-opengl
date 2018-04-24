00427e9e0b7b49db9b05abc468aee7ba3eeaa399
Faizan Khan <faizankhan@lawn-128-61-7-57.lawn.gatech.edu>
2018-04-24 11:05:51 -0400

    Solar System

---
+++ cmake-build-debug/CMakeFiles/SolarSystem.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SolarSystem.dir/solarsystem.cpp.o"
+  "CMakeFiles/SolarSystem.dir/glad.c.o"
+  "SolarSystem.pdb"
+  "SolarSystem"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/SolarSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/faizankhan/CLionProjects/solarsystem-opengl/glfw/src/cocoa_init.m" "/Users/faizankhan/CLionProjects/solarsystem-opengl/cmake-build-debug/glfw/src/CMakeFiles/glfw.dir/cocoa_init.m.o"
+  "/Users/faizankhan/CLionProjects/solarsystem-opengl/glfw/src/cocoa_joystick.m" "/Users/faizankhan/CLionProjects/solarsystem-opengl/cmake-build-debug/glfw/src/CMakeFiles/glfw.dir/cocoa_joystick.m.o"
+  "/Users/faizankhan/CLionProjects/solarsystem-opengl/glfw/src/cocoa_monitor.m" "/Users/faizankhan/CLionProjects/solarsystem-opengl/cmake-build-debug/glfw/src/CMakeFiles/glfw.dir/cocoa_monitor.m.o"
+  "/Users/faizankhan/CLionProjects/solarsystem-opengl/glfw/src/cocoa_time.c" "/Users/faizankhan/CLionProjects/solarsystem-opengl/cmake-build-debug/glfw/src/CMakeFiles/glfw.dir/cocoa_time.c.o"
+  "/Users/faizankhan/CLionProjects/solarsystem-opengl/glfw/src/cocoa_window.m" "/Users/faizankhan/CLionProjects/solarsystem-opengl/cmake-build-debug/glfw/src/CMakeFiles/glfw.dir/cocoa_window.m.o"
+  "/Users/faizankhan/CLionProjects/solarsystem-opengl/glfw/src/context.c" "/Users/faizankhan/CLionProjects/solarsystem-opengl/cmake-build-debug/glfw/src/CMakeFiles/glfw.dir/context.c.o"
+  "/Users/faizankhan/CLionProjects/solarsystem-opengl/glfw/src/egl_context.c" "/Users/faizankhan/CLionProjects/solarsystem-opengl/cmake-build-debug/glfw/src/CMakeFiles/glfw.dir/egl_context.c.o"
+  "/Users/faizankhan/CLionProjects/solarsystem-opengl/glfw/src/init.c" "/Users/faizankhan/CLionProjects/solarsystem-opengl/cmake-build-debug/glfw/src/CMakeFiles/glfw.dir/init.c.o"
+  "/Users/faizankhan/CLionProjects/solarsystem-opengl/glfw/src/input.c" "/Users/faizankhan/CLionProjects/solarsystem-opengl/cmake-build-debug/glfw/src/CMakeFiles/glfw.dir/input.c.o"
+  "/Users/faizankhan/CLionProjects/solarsystem-opengl/glfw/src/monitor.c" "/Users/faizankhan/CLionProjects/solarsystem-opengl/cmake-build-debug/glfw/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/Users/faizankhan/CLionProjects/solarsystem-opengl/glfw/src/nsgl_context.m" "/Users/faizankhan/CLionProjects/solarsystem-opengl/cmake-build-debug/glfw/src/CMakeFiles/glfw.dir/nsgl_context.m.o"
+  "/Users/faizankhan/CLionProjects/solarsystem-opengl/glfw/src/osmesa_context.c" "/Users/faizankhan/CLionProjects/solarsystem-opengl/cmake-build-debug/glfw/src/CMakeFiles/glfw.dir/osmesa_context.c.o"
+  "/Users/faizankhan/CLionProjects/solarsystem-opengl/glfw/src/posix_thread.c" "/Users/faizankhan/CLionProjects/solarsystem-opengl/cmake-build-debug/glfw/src/CMakeFiles/glfw.dir/posix_thread.c.o"
+  "/Users/faizankhan/CLionProjects/solarsystem-opengl/glfw/src/vulkan.c" "/Users/faizankhan/CLionProjects/solarsystem-opengl/cmake-build-debug/glfw/src/CMakeFiles/glfw.dir/vulkan.c.o"
+  "/Users/faizankhan/CLionProjects/solarsystem-opengl/glfw/src/window.c" "/Users/faizankhan/CLionProjects/solarsystem-opengl/cmake-build-debug/glfw/src/CMakeFiles/glfw.dir/window.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_GLFW_USE_CONFIG_H"
+  "_XOPEN_SOURCE=600"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../glfw/include"
+  "../glad/include"
+  "../glfw/src"
+  "glfw/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
