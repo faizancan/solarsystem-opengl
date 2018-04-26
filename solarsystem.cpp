d3d06bc5b80a6fb96bf197e1b2bcdc2457766de5
Faizan Khan <faizankhan@lawn-128-61-7-57.lawn.gatech.edu>
2018-04-25 22:23:24 -0400

    basic functionality for sun/planetary orbits and rotation. should be enough to start work on more complex graphics (for now every planet is a green 2d polygon). will tweak the tilts later

--- solarsystem.cpp
+++ solarsystem.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <sys/stat.h>
 #include <GLUT/glut.h>  // GLUT, include glu.h and gl.h
+#include "solarsystem.h"
 
 // global variable
 GLfloat angle = 0.0f;  // rotational angle of the shapes
-int refreshMills = 30; // refresh interval in milliseconds
+GLint refreshMills = 30; // refresh GLinterval in milliseconds
+GLfloat earthDays = 0.0f;
+GLfloat earthDaysPerYear = 0.0f;
+GLfloat earthTimeInterval = 0.5f;
+
+void displaySun();
+void displayPlanets();
+void displayEachPlanet(GLfloat tilt, GLfloat distanceFromSun, GLfloat rotationPeriod, GLfloat orbitPeriod, GLfloat color);
 
 /* Initialize OpenGL Graphics */
 void initGL() {
@@ -13,19 +21,26 @@ void initGL() {
 }
 
 /* Called back when timer expired */
-void Timer(int value) {
-    glutPostRedisplay();      // Post re-paint request to activate display()
-    glutTimerFunc(refreshMills, Timer, 0); // next Timer call milliseconds later
+void Timer(GLint value) {
+
+    earthDays += earthTimeInterval;
+    earthDaysPerYear += earthTimeInterval;
+    if (earthDaysPerYear == 365)
+        earthDays = 0;
+    glutPostRedisplay();      // Post re-paGLint request to activate display()
+    glutTimerFunc(refreshMills, Timer, 1); // next Timer call milliseconds later
 }
 
-/* Handler for window-repaint event. Call back when the window first appears and
-   whenever the window needs to be re-painted. */
+/* Handler for window-repaGLint event. Call back when the window first appears and
+   whenever the window needs to be re-paGLinted. */
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);   // Clear the color buffer
     glMatrixMode(GL_MODELVIEW);     // To operate on Model-View matrix
     glLoadIdentity();               // Reset the model-view matrix
 
     // display functions go here
+    displaySun();
+    displayPlanets();
 
     glutSwapBuffers();   // Double buffered - swap the front and back buffers
 
@@ -33,9 +48,74 @@ void display() {
     angle += 2.0f;
 }
 
+void displaySun() {
+    glPushMatrix();
+
+    glRotatef(SUN_TILT, 0.0f, 0.0f, 1.0f); // axis is tilted  6 degrees
+    glRotatef(360.0f * earthDays / 24.0, 0.0f, 1.0f, 0.0f); //
+
+
+    // this animation is filler
+    glBegin(GL_POLYGON);
+    glColor3f(1.0f, 1.0f, 0.0f); // YELLOW SUN
+    glVertex2f(-0.01f, -0.02f);
+    glVertex2f( 0.01f, -0.02f);
+    glVertex2f( 0.02f,  0.00f);
+    glVertex2f( 0.01f,  0.02f);
+    glVertex2f(-0.01f,  0.02f);
+    glVertex2f(-0.02f,  0.00f);
+    glEnd();
+
+    glPopMatrix();
+}
+
+void displayPlanets() {
+    displayEachPlanet(MERCURY_TILT,
+                      MERCURY_DISTANCE, MERCURY_ROTATION, MERCURY_ORBIT, 0.1);
+    displayEachPlanet(VENUS_TILT,
+                      VENUS_DISTANCE, VENUS_ROTATION, VENUS_ORBIT, 0.2);
+    displayEachPlanet(EARTH_TILT,
+                      EARTH_DISTANCE, EARTH_ROTATION, EARTH_ORBIT, 0.3);
+    displayEachPlanet(MARS_TILT,
+                      MARS_DISTANCE, MARS_ROTATION, MARS_ORBIT, 0.4);
+    displayEachPlanet(JUPITER_TILT,
+                      JUPITER_DISTANCE, JUPITER_ROTATION, JUPITER_ORBIT, 0.5);
+    displayEachPlanet(SATURN_TILT,
+                      SATURN_DISTANCE, SATURN_ROTATION, SATURN_ORBIT, 0.6);
+    displayEachPlanet(URANUS_TILT,
+                      URANUS_DISTANCE, URANUS_ROTATION, URANUS_ORBIT, 0.7);
+    displayEachPlanet(NEPTUNE_TILT,
+                      NEPTUNE_DISTANCE, NEPTUNE_ROTATION, NEPTUNE_ORBIT, 0.8);
+    displayEachPlanet(PLUTO_TILT,
+                      PLUTO_DISTANCE, PLUTO_ROTATION, PLUTO_ORBIT, 0.9);
+
+}
+
+// instead of bitmap field, we can use the color field
+void displayEachPlanet(GLfloat tilt, GLfloat distanceFromSun, GLfloat rotationPeriod, GLfloat orbitPeriod, GLfloat color) {
+    glPushMatrix();
+
+    glRotatef(tilt, 0.0f, 0.0f, 1.0f);
+    glRotatef(360.0f * earthDaysPerYear / orbitPeriod, 0.0f, 1.0f, 0.0f);
+    glTranslatef(distanceFromSun, 0.0f, 0.0f);
+    glRotatef(360.0f * earthDays / rotationPeriod, 0.0f, 1.0f, 0.0f);
+
+    glBegin(GL_POLYGON);
+    glColor3f(color, 1.0f / color, 0.0f);
+    glVertex2f(-0.01f, -0.02f);
+    glVertex2f( 0.01f, -0.02f);
+    glVertex2f( 0.02f,  0.00f);
+    glVertex2f( 0.01f,  0.02f);
+    glVertex2f(-0.01f,  0.02f);
+    glVertex2f(-0.02f,  0.00f);
+    glEnd();
+
+    glPopMatrix();
+}
+
 /* Handler for window re-size event. Called back when the window first appears and
    whenever the window is re-sized with its new width and height */
-void reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integer
+void reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative GLinteger
     // Compute aspect ratio of the new window
     if (height == 0) height = 1;                // To prevent divide by 0
     GLfloat aspect = (GLfloat)width / (GLfloat)height;
@@ -55,22 +135,22 @@ void reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integ
     }
 }
 
-void specialKeys(int key, int x, int y) {
+void specialKeys(GLint key, GLint x, GLint y) {
     switch (key) {
-        case GLUT_KEY_UP:    // for pressing the up key
-
+        case GLUT_KEY_UP:    // for pressing the up key, increases earth day speed
+            earthTimeInterval *= 2;
             break;
     }
 }
 
 /* Main function: GLUT runs as a console application starting at main() */
-int main(int argc, char** argv) {
+GLint main(GLint argc, char** argv) {
     glutInit(&argc, argv);          // Initialize GLUT
     glutInitDisplayMode(GLUT_DOUBLE);  // Enable double buffered mode
     glutInitWindowSize(640, 480);   // Set the window's initial width & height - non-square
     glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
     glutCreateWindow("Solar System");  // Create window with the given title
-    glutDisplayFunc(display);       // Register callback handler for window re-paint event
+    glutDisplayFunc(display);       // Register callback handler for window re-paGLint event
     glutReshapeFunc(reshape);       // Register callback handler for window re-size event
     glutTimerFunc(0, Timer, 0);     // First timer call immediately
     glutSpecialFunc(specialKeys); // Register callback handler for special-key event
---
+++ solarsystem.h
@@ -0,0 +1,50 @@
+//
+// Solar System
+//
+#ifndef SOLARSYSTEM_H
+
+// tilt from y axis in degrees
+const GLfloat SUN_TILT = 7;
+const GLfloat MERCURY_TILT = 0;
+const GLfloat VENUS_TILT = 3;
+const GLfloat EARTH_TILT = 23;
+const GLfloat MARS_TILT = 25;
+const GLfloat JUPITER_TILT = 3;
+const GLfloat SATURN_TILT = 27;
+const GLfloat URANUS_TILT = 82;
+const GLfloat NEPTUNE_TILT = 28;
+const GLfloat PLUTO_TILT = 60;
+
+// orbit radius, AU scaled by 4.0f
+const GLfloat MERCURY_DISTANCE = 0.1f;
+const GLfloat VENUS_DISTANCE = 0.18f;
+const GLfloat EARTH_DISTANCE = 0.25f;
+const GLfloat MARS_DISTANCE = 0.38f;
+const GLfloat JUPITER_DISTANCE = 1.3;
+const GLfloat SATURN_DISTANCE = 2.38;
+const GLfloat URANUS_DISTANCE = 4.8;
+const GLfloat NEPTUNE_DISTANCE = 7.5f;
+const GLfloat PLUTO_DISTANCE = 10.0f;
+
+const GLfloat MERCURY_ORBIT = 88;
+const GLfloat VENUS_ORBIT = 225;
+const GLfloat EARTH_ORBIT = 365;
+const GLfloat MARS_ORBIT = 687;
+const GLfloat JUPITER_ORBIT = 4380;
+const GLfloat SATURN_ORBIT = 10585;
+const GLfloat URANUS_ORBIT = 30660;
+const GLfloat NEPTUNE_ORBIT = 60225;
+const GLfloat PLUTO_ORBIT = 90520;
+
+const GLfloat MERCURY_ROTATION = 59;
+const GLfloat VENUS_ROTATION = 243;
+const GLfloat EARTH_ROTATION = 1;
+const GLfloat MARS_ROTATION = 1;
+const GLfloat JUPITER_ROTATION = 0.42;
+const GLfloat SATURN_ROTATION = 0.46;
+const GLfloat URANUS_ROTATION = 0.71;
+const GLfloat NEPTUNE_ROTATION = 0.67f;
+const GLfloat PLUTO_ROTATION = 6.39;
+
+#define SOLARSYSTEM_H
+#endif
